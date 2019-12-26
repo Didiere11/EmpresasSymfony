58defba9e4f8eca7cba30b155c25760f9067c2e1
Didiere11 <ramirezrenteriajosedidiere@gmail.com>
2019-12-26 12:17:04 -0600

    Principal

---
+++ src/EmpresasBundle/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace EmpresasBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="Empresas_home")
+     */
+    public function indexAction()
+    {
+        return $this->render('EmpresasBundle:Default:index.html.twig');
+    }
+    /**
+     * @Route("/login", name="Empresas_login")
+     */
+    public function loginAction()
+    {
+        return $this->render('EmpresasBundle:Default:login.html.twig');
+    }
+}
