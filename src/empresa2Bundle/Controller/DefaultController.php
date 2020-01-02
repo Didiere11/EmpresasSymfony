2b2ae33f9c9acd3530672f1d2e2b95dd12e56f17
Didiere11 <ramirezrenteriajosedidiere@gmail.com>
2020-01-02 11:03:22 -0600

    nueva modificacion

--- src/empresa2Bundle/Controller/DefaultController.php
+++ src/empresa2Bundle/Controller/DefaultController.php
@@ -6,73 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Utilerias\SQLBundle\Model\SQLModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use empresa2Bundle\Model\EmpresaModel;
 
 class DefaultController extends Controller
 {
-    protected $SQLPrueba;
+    protected $EmpresaModel;
 
     public function __construct() {
-        $this->SQLPrueba = new SQLModel();
+        $this->EmpresaModel = new EmpresaModel();
        
-    }
-
-    public function indexAction(Request $request)
-    {
-
-        return $this->render('empresa2Bundle:Default:index.html.twig');
     }
     protected function jsonResponse($data) {
         $response = new Response(json_encode($data));
         $response->headers->set('Content-Type', 'application/json');
         return $response;
     }
-    public function loginAction(Request $request)
-    {        //validacion de metodo
-        if ($request->getMethod() == 'POST') {
-            //extraccion de parametros
-            $post = $request->request->all();
-
-            //validacion de usuarios
-            $query = 'SELECT correo,contraseña FROM usuario';
-            $query .= ' WHERE correo= ' ."'" .$post["correo"]."'";
-            $query .= ' AND contraseña=' ."'".$post['contra']."'";
-            $result = $this->SQLPrueba->executeQuery($query);
-<<<<<<< HEAD
-=======
-
-          
->>>>>>> 785d21ebe479bcb239adffb3ecbd0bfff83aebb6
-           /*if ($result['data'] == null) {
-               print_r('no entra');
-                die();
-             }else{
-                print_r('si entra');
-                die();
-<<<<<<< HEAD
-                 
-             }*/
-          // print_r($result);
-=======
-             }*/
-          // print_r($insert);
->>>>>>> 785d21ebe479bcb239adffb3ecbd0bfff83aebb6
-           //die();
-        }
-        return $this->render('empresa2Bundle:Default:login.html.twig');
+    public function IndexAction(Request $request){
+       
+        return $this->render('empresa2Bundle:Default:index.html.twig');
+    }
+    public function loginAction(Request $request){
+        $result = $this->EmpresaModel->getEmpresas();
+        $empresa = $result['data'];
+        $content['empresa'] = $empresa;
+       
+        return $this->render('empresa2Bundle:Default:catalogempresas.html.twig', array('content' => $content));
     }
 
-    public function insertusrAction(Request $request){
-        if ($request->getMethod() == 'POST') {
-            //extraccion de parametros
-            $post = $request->request->all();
-            $insert = 'INSERT INTO usuario ' ;
-            $insert .= '(nomusuario,correo,contraseña,domicilio,idtipousr)';
-            $insert .= 'VALUES ('."'".$post['nom']."'".','."'".$post['corr']."'".','."'".$post['pwd']."'".','."'".$post['dom']."'".',1)';
-            
-        
-            $result = $this->SQLPrueba->executeQuery($insert);
-        }
-        return jsonResponse($result);
-    }
+   
    
 }
